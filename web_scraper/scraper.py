1058e8a1d78e861ee0444b3aed5ac0796e7aeb74
Jamison French <jjfrench@Jamisons-MacBook-Pro.local>
2020-11-28 14:18:38 -0600

    Progress

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -8,32 +8,35 @@ from bs2json import bs2json
 import time
 import json
 
+TICKERS = [
+    'BEEM',
+    'AMD',
+    'REGI',
+    'LOGI',
+    'TSLA'
+]
+
 def main():
     start_time = time.time()
     session = FuturesSession(executor=ThreadPoolExecutor(max_workers=10))
-    TICKERS = [
-        'BEEM',
-        'AMD',
-        'REGI',
-        'LOGI'
-    ]
-
+    
     futures = [
         session.get(f'https://www.finviz.com/quote.ashx?t={TICKER}', headers={'user-agent': 'scrapper'}) 
             for TICKER in TICKERS
         ]
 
+    data = []
     for index, future in enumerate(as_completed(futures)):
         html = future.result().text
         soup = BeautifulSoup(html,'lxml').find_all(id='news-table')
-        _json = bs2json().convertAll(soup)
-        with open('data.txt', 'w') as output:
-            json.dump(_json[index], output, indent=2)
-        break
-        
+        for item in soup:
+            soup = item.find_all('a')
+            _json = bs2json().convertAll(soup)
+            data.append(_json)
+    with open('data.txt', 'w') as output:
+        json.dump(data, output, indent=2)
 
     print("--- %s seconds ---" % (time.time() - start_time))
     
-
 if __name__ == "__main__":
     main()
