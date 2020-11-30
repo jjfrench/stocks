b65379d8b5e5a084914b8d411bee78f8e6f06094
Jamison French <jamisonjohnfrench@gmail.com>
2020-11-29 21:30:05 -0600

    Progress

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -2,7 +2,6 @@
 from requests import get
 from requests_futures.sessions import FuturesSession
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from pprint import pprint
 from bs4 import BeautifulSoup
 from bs2json import bs2json
 import time
@@ -23,26 +22,32 @@ def finviz():
     start_time = time.time()
     session = FuturesSession(executor=ThreadPoolExecutor(max_workers=10))
     
-    futures = [
-        session.get(f'https://www.finviz.com/quote.ashx?t={TICKER}', headers={'user-agent': 'scrapper'}) 
-            for TICKER in TICKERS
-        ]
+    # futures = [
+    #     session.get(f'https://www.finviz.com/quote.ashx?t={TICKER}', headers={'user-agent': 'scrapper'}) 
+    #         for TICKER in TICKERS
+    #     ]
+    futures = []
+    for TICKER in TICKERS:
+        future = session.get(f'https://www.finviz.com/quote.ashx?t={TICKER}', headers={'user-agent': 'scrapper'})
+        future.tick = TICKER
+        futures.append(future)
 
-    data = []
+    data = {'stocks': []}
     for future in as_completed(futures):
+        print(future.tick)
         html = future.result().text
         soup = BeautifulSoup(html,'lxml').find_all(id='news-table')
         for item in soup:
             soup = item.find_all('a')
             _json = bs2json().convertAll(soup)
-            data.append(_json)
+            data['stocks'].append({f'{future.tick}':_json})
     with open('data.txt', 'w') as output:
         json.dump(data, output, indent=2)
 
-    print("--- %s seconds ---" % (time.time() - start_time))
+    print('--- %s seconds ---' % (time.time() - start_time))
 
 def main():
     finviz()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
